a9718c372890b2953f36a9e6516f2048b70decf3
Marianne Linardos <mlinardos.edu@gmail.com>
2019-08-02 16:14:18 -0400

    Add project files.

---
+++ Assignment-GenericCollections/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Assignment_GenericCollections {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
